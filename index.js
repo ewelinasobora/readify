a8a51ea422db48be26f2a2221b5d28500b7a98b8
Ewelina Sobora <e@e-dot.uk>
2023-12-15 14:10:53 +0000

    Add core functionality to initialize program

--- index.js
+++ index.js
@@ -59,10 +59,31 @@ const requirements = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+  fs.appendFile(fileName, data, (err) => {
+    if (err) throw err;
+    JSON.stringify(data)
+    console.log("Data is appended to file successfully.");
+  });
+};
 
 // function to initialize program
 function init() {
+  inquirer
+    .prompt(
+      requirements
+    )
+    .then((data) => {
+      const fileName = process.argv[2];
+      const generatedData = generateMarkdown(data);
+      writeToFile(fileName, generatedData)
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
 }
 
 // function call to initialize program
