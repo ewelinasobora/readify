5e00475da17cff05c5d8ad85ddb812b8c3dcb45d
Ewelina Sobora <e@e-dot.uk>
2023-12-15 12:24:41 +0000

    Add array of requiremnents for user input

--- index.js
+++ index.js
@@ -3,9 +3,58 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
-
+// array of requirements for user input
+const requirements = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of the project:"
+  },
+  {
+    type: "input",
+    name: "sections",
+    message: "Enter the sections entitled (comma-separated):"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter the description:"
+  },
+  {
+    type: "input",
+    name: "tableOfContents",
+    message: "Enter the table of contents:"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter the installation instructions:"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the usage information:"
+  },
+  {
+    type: "input",
+    name: "licence",
+    message: "Enter the licence:"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contributing guidelines:"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter the test instructions:"
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Enter the questions:"
+  }
 ];
 
 // function to write README file
@@ -14,7 +63,6 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
 }
 
 // function call to initialize program
